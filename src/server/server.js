42b4445c0b5a4cd5368ec84fe374873ceb3f2222
nrzngr <109433343+nrzngr@users.noreply.github.com>
2024-06-03 09:27:50 +0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 const init = async () => {
 	const server = Hapi.server({
-		port: process.env.PORT || 5000,
+		port: process.env.PORT || 8080,
 		host: "0.0.0.0",
 		routes: {
 			cors: true,
